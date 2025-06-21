656b45ae22c13d69abc3572eaadc3fd7ad3e5e5a
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-06-21 20:19:00 +0300

    Time: 36 ms (66.93%), Space: 18.5 MB (81.64%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,6 +9,19 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        /*
+         * Length of list A before intersection: a
+         * Length of list B before intersection: b
+         * Length of shared (intersecting) part: c
+         * Total: A: a + c, B: b + c
+         * When x reaches the end of list A, it jumps to headB.
+         * When y reaches the end of list B, it jumps to headA.
+         * They traverse the same total number of nodes: 
+         * a + c + b == b + c + a
+         * Therefore, they meet either:
+                - At the intersection node, or
+                - At NULL, if there's no intersection. 
+         */
         ListNode* x = headA;
         ListNode* y = headB;
         while(x!=y){
