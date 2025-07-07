fdf836efa095b1640d0b0aebb443fe947f7612f3
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-07-07 12:35:35 +0300

    Time: 1 ms (54.62%), Space: 9.7 MB (41.08%) - LeetHub

---
+++ 3600-find-the-k-th-character-in-string-game-i/3600-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+        string s = "a";
+        --k;
+        char prev = 'a';
+        while(s.size() <= k){
+            string t = s;
+            for(int i = 0; i < t.size(); i++){
+                t[i]++;
+            }
+            s += t;
+        }
+        return s[k];
+    }
+};
