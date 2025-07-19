1dcf7aaf8316270ce1703f1aa698779bad999768
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-07-19 11:39:20 +0300

    Time: 62 ms (64.35%), Space: 14.5 MB (45.45%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int ans = 0;
+        vector<int> dp(2510, 1);
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
