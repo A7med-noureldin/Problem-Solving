aacea4407e30f4edb548d001c069e6ca2114e151
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-08-01 12:14:44 +0300

    Time: 4 ms (79.51%), Space: 14 MB (94.59%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -2,15 +2,28 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int ans = 0;
-        vector<int> dp(2510, 1);
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < i; j++){
-                if(nums[j] < nums[i]){
-                    dp[i] = max(dp[i], dp[j]+1);
+        vector<int> v;
+        v.push_back(nums[0]);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] <= v.back()){
+                int l = 0, r = v.size()-1;
+                while(l <= r){
+                    int mid = (l + r)/2;
+                    if(v[mid] == nums[i]){
+                        l = mid;
+                        break;
+                    }
+                    else if(v[mid] > nums[i]) r = mid-1;
+                    else l = mid+1;
                 }
+                v[l] = nums[i];
             }
-            ans = max(ans, dp[i]);
+            else{
+                v.push_back(nums[i]);
+                ans = max(ans, (int)v.size());
+            }
         }
+        ans = max(ans, (int)v.size());
         return ans;
     }
 };
