871186066f46e7a978b99c4960588cbecaaf8c36
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-08-03 12:20:02 +0300

    Time: 217 ms (22.26%), Space: 8.8 MB (11.52%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    int dp[31];
-   Solution() {
-        dp[1] = 1;
-    }
     int fib(int n) {
+        int dp[31] = {};
         if(n <= 0) return 0;
+        if(n == 1)return 1;
         if(dp[n] > 0) return dp[n];
         dp[n] = fib(n - 1) + fib(n - 2);
         return dp[n];
