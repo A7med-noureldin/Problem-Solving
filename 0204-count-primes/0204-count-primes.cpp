59287e165d85fcfa7db274da4e77c6169c73e7de
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-09-23 14:14:24 +0300

    Time: 824 ms (5.09%), Space: 315.2 MB (8.83%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -3,17 +3,16 @@ class Solution {
     vector<int> primes; 
 public:
     Solution() : primes(N, 1) {}
-    int ans = 0;
     int seive(int n){
         primes[0] = primes[1] = 0;
+        for(int i = 2; i*i < n; i++){
+                for(long long j = i*i; j < n; j+=i)
+                    primes[j] = 0;
+        }
+        int ans = 0;
         for(int i = 2; i < n; i++){
-            if(primes[i]){
+            if(primes[i])
                 ans++;
-                if ((long long)i * i < n){
-                    for(long long j = i*i; j < n; j+=i)
-                        primes[j] = 0;
-                }
-            }
         }
         return ans;
     }
