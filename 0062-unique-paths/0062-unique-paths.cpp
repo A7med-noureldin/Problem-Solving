2b7743b9d58f1fd6f39016ed3f7a781e74f0cd3e
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-11-29 23:44:43 +0200

    Time: 0 ms (100%), Space: 9.4 MB (49.05%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        const int mod = 2e9;
+    
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i > 0 && j > 0){
+                    dp[i][j] = (dp[i-1][j] + dp[i][j-1])%mod;
+                }
+                else if(i == 0 && j > 0) dp[i][j] = dp[i][j-1];
+                else if(i > 0 && j == 0) dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1] % mod;
+    }
+};
