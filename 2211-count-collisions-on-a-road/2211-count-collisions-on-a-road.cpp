352f76477a94b4b8074f13afd4d62f8c6758a887
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-12-04 14:28:22 +0200

    Time: 9 ms (72.46%), Space: 19.1 MB (84.64%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.size(), ans = 0;
+        int i = 0, j = n-1;
+
+        while (i < n && directions[i] == 'L') i++;
+        while (j >= 0 && directions[j] == 'R') j--;
+
+        for (int k = i; k <= j; k++) {
+            if (directions[k] != 'S')
+                ans++;
+        }
+        return ans;
+    }
+};
