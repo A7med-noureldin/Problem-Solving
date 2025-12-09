cbb0e53b39037c7d5ceb83bc105a0c573e4991e2
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-12-09 18:18:33 +0200

    Time: 561 ms (75.55%), Space: 310.1 MB (91.61%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        map<int, int> freq1, freq2;
-        for(int i = 1; i < nums.size(); i++){
+        int MAX = 200000+5;
+        const int MOD = 1e9+7;
+        vector<int> freq1(MAX), freq2(MAX);
+        int n = nums.size();
+        for(int i = 1; i < n; i++)
             freq2[nums[i]]++;
-        }
+
         long long ans = 0;
-    
-        for(int i = 1; i+1 < nums.size(); i++){
-            freq1[nums[i-1]]++, freq2[nums[i]]--;
-            int x = 2*nums[i];
-            ans = (ans + 1LL * freq1[x] * freq2[x]) % 1000000007;
+
+        for(int i = 1; i+1 < n; i++) {
+            freq1[nums[i-1]]++;
+            freq2[nums[i]]--;
+
+            int x = 2 * nums[i];
+            ans = (ans + 1LL * freq1[x] * freq2[x]) % MOD;
         }
         return ans;
     }
