024eab70f4c16a71c7d0a2377565c705ca89bb8f
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2025-12-30 01:32:13 +0200

    Time: 0 ms (100%), Space: 99.2 MB (50.15%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -6,13 +6,11 @@ public:
         for(int i = 0; i < n; i++){
             mp[edges[i][0]]++;
             mp[edges[i][1]]++;
+
+            if(mp[edges[i][0]] > 1) return edges[i][0];
+            if(mp[edges[i][1]] > 1) return edges[i][1];    
         }
         
-        for(auto [p, q] : mp){
-            if(q == n){
-                return p;
-            }
-        }
         return -1;
     }
 };
