3afd31483575d2be7aab20cb75359e94389aca4f
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-01-06 21:44:12 +0200

    Time: 0 ms (100%), Space: 109.5 MB (30.1%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -11,22 +11,43 @@
  */
 class Solution {
 public:
-    map<int, long long> mp;
-    void dfs(TreeNode *x, int lvl){
-        if(!x) return;
-        mp[lvl] += x->val;
-        dfs(x->left, lvl+1);
-        dfs(x->right, lvl+1);
-    }
+    // map<int, long long> mp;
+    // void dfs(TreeNode *x, int lvl){
+    //     if(!x) return;
+    //     mp[lvl] += x->val;
+    //     dfs(x->left, lvl+1);
+    //     dfs(x->right, lvl+1);
+    // }
     int maxLevelSum(TreeNode* root) {
         if(!root->left && !root->right) return root->val;
-        dfs(root, 1);
-        int ans = 0, maxi = INT_MIN;
-        for(auto [p, q] : mp){
-            if(q > maxi){
-                maxi = q;
-                ans = p;
+        // dfs(root, 1);
+        // int ans = 0, maxi = INT_MIN;
+        // for(auto [p, q] : mp){
+        //     if(q > maxi){
+        //         maxi = q;
+        //         ans = p;
+        //     }
+        // }
+        // return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        int currLvl = 1, maxi = INT_MIN, ans = 0;
+        while(!q.empty()){
+            int siz = q.size();
+            int sum = 0;
+
+            while(siz--){
+                TreeNode *p  = q.front();
+                q.pop();
+                sum += p->val;
+                if(p->left != NULL) q.push(p->left);
+                if(p->right != NULL) q.push(p->right);
             }
+            if(sum > maxi){
+                maxi = sum, ans = currLvl;
+            }
+            currLvl++;
         }
         return ans;
     }
