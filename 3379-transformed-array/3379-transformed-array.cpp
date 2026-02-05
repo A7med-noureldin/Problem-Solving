48fce38a1040056494ba353b7eb3b65b5c7c2beb
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-02-05 16:36:14 +0200

    Time: 8 ms (46.52%), Space: 25.6 MB (91.36%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 0);
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0) continue;
+            int pos = (i+nums[i])%n;
+            if(pos < 0) pos += n;
+            res[i] = nums[pos];
+        }
+        return res;
+    }
+};
