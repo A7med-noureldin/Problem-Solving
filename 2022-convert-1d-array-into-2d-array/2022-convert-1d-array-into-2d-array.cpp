a2d59acc190083e31c50afd497d34bde80da7732
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-02-09 11:19:35 +0200

    Time: 12 ms (51.69%), Space: 89.8 MB (80.22%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> ans;
+        int orgSize = original.size();
+        if(orgSize != m*n) return ans;
+
+        ans = vector<vector<int>>(m, vector<int>(n));
+        // Brute force
+        int x = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                ans[i][j] = original[x++];
+            }
+        }
+        return ans;
+    }
+};
