967a66b2998654ca50ff1c0c2670bd1869242b87
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-03-04 07:38:15 +0200

    Time: 4 ms (10.73%), Space: 17.1 MB (41.15%) - LeetHub

---
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        vector<int> col(m, 0), row(n, 0);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                col[j] += (mat[i][j] == 1);
+                row[i] += (mat[i][j] == 1);
+            }
+        }
+
+        int ans = 0;
+        for(int i = 0; i < n; i++){ 
+            for(int j = 0; j < m; j++){
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
