839473626ec266f904882b77ada3bcf669e1289b
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-03-29 15:57:59 +0200

    Time: 0 ms (100%), Space: 8.4 MB (49.03%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    bool canBeEqual(string s, string t) {
-        map<char, int> even, odd;
-        for(int i = 0; i < 4; i++){
-            if(i&1) odd[s[i]]++;
-            else even[s[i]]++;
+    bool canBeEqual(string s, string t) { 
+        for(int i = 0; i < 2; i++){
+            if(s[i] != t[i]) swap(s[i], s[i+2]);
         }
-        for(int i = 0; i < 4; i++){
-            if(i&1){
-                if(odd[t[i]] > 0) {
-                    odd[t[i]]--;
-                    continue;
-                }
-                return false;
-            }
-            else{
-                if(even[t[i]] > 0){
-                    even[t[i]]--;
-                    continue;
-                }
-                return false;
-            }
-        }
-        return true;
+        
+        return s == t;
     }
 };
