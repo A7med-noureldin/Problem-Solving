f103da8ee5988c8b11ed451845a6a4e43c94109b
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-06-11 04:11:18 +0300

    Time: 0 ms (100%), Space: 11.7 MB (47.42%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int x = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != val){
-                nums[x++] = nums[i];
+        int k = nums.size(), n = nums.size();
+        queue<int> q;
+        for(int i = 0; i < n; i++){
+            if(nums[i]==val){
+                k--; q.push(i);
+            }
+            else if(!q.empty()){
+                int x = q.front(); q.pop();
+                q.push(i);
+                nums[x] = nums[i];
             }
         }
-        return x;
+        return k;
     }
 };
