250762b8890fbc9a26e11335a54e5baf7023c29d
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-06-13 21:12:25 +0300

    Time: 0 ms (100%), Space: 10.6 MB (43.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,12 +3,12 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         if(n == 1) return nums[0];
-        
-        vector<int> dp(nums.begin(), nums.end());
-        dp[1] = max(nums[0], nums[1]);
+        else if(n == 2) return max(nums[0], nums[1]);
+        vector<int> dp(n, 0);
+        dp[0] = nums[0], dp[1] = max(nums[1], nums[0]);
         for(int i = 2; i < n; i++){
-            dp[i] = max(dp[i-1], dp[i-2]+dp[i]);
+            dp[i] += max(dp[i-2] + nums[i], dp[i-1]);
         }
-        return dp[n-1];
+        return (int) *max_element(dp.begin(), dp.end());
     }
 };
