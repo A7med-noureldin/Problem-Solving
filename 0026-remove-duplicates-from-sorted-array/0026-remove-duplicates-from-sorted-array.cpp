907b47d666f7591c32ce222f0234fdceebcb0705
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-06-13 19:52:42 +0300

    Time: 0 ms (100%), Space: 22.6 MB (79.67%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int x = 0;
-        vector<int> vis(400, 0);
-        for(int i = 0; i < nums.size(); i++){
-            if(vis[nums[i]+100] == 0)nums[x++] = nums[i],  vis[nums[i]+100]++;
+        if(nums.size() == 1) return 1;
+
+        int idx = 1, n = (int)nums.size();
+        for(int j = 1; j < n; j++){
+            if(nums[idx-1] != nums[j]){
+                nums[idx] = nums[j], idx++;
+            }
         }
-        return x;
+        return idx;
     }
 };
