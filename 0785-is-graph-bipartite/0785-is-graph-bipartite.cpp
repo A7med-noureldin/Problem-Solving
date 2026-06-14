f6b82f14f3f6e6007010c9f56e10785e79716769
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-06-14 09:04:44 +0300

    Time: 0 ms (100%), Space: 17.6 MB (40.6%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     vector<int> color;
-    vector<int> adj[105];
-    bool isbipartite;
-    void dfs(int u, int c){
-        if(!isbipartite) return;
+    bool ok = true;
+
+    void dfs(int u, int c, vector<vector<int>>& graph) {
         color[u] = c;
-        for(auto v : adj[u]){
-            if(color[v] == -1) dfs(v, 1-c);
-            else if (c == color[v]) isbipartite = false;
+
+        for (int v : graph[u]) {
+            if (color[v] == -1)
+                dfs(v, 1 - c, graph);
+            else if (color[v] == c)
+                ok = false;
         }
     }
 
     bool isBipartite(vector<vector<int>>& graph) {
-        color.assign(graph.size(), -1);
-        isbipartite = true;
-        for(int i = 0; i < graph.size(); i++){
-            for(int j = 0; j < graph[i].size(); j++){
-                adj[i].push_back(graph[i][j]);
-                adj[graph[i][j]].push_back(i);
-            }
+        int n = graph.size();
+        color.assign(n, -1);
+
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1)
+                dfs(i, 0, graph);
         }
 
-        for(int i = 0; i < graph.size(); i++){
-            if(color[i] == -1) {
-                dfs(i, 0);
-            }
-        }
-        return isbipartite;
+        return ok;
     }
 };
