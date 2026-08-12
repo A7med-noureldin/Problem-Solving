ff161e8be44dfe27017f012dfb77c8c6a3bd59e5
A7med-noureldin <162290003+A7med-noureldin@users.noreply.github.com>
2026-08-12 15:25:31 +0300

    Time: 274 ms (5.22%), Space: 293.4 MB (5.11%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int maxSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size();
         vector<int> v(nums.begin(), nums.end());
         sort(v.begin(), v.end());
         v.erase(unique(v.begin(), v.end()), v.end());
-        vector<int> freq(1e5+5, 0);
-        int l = 0, r = 0, n = nums.size();
+        vector<int> freq(1e5+5, 0), id(n, -1);
+        for(int i = 0; i < n; i++){
+            id[i] = lower_bound(v.begin(), v.end(), nums[i]) - v.begin();
+        }
+        int l = 0, r = 0;
         int ans = 0;
         for(int r = 0; r < n; r++){
-            int cur = lower_bound(v.begin(), v.end(), nums[r]) - v.begin();
+            int cur = id[r];
             freq[cur]++;
             while(l <= r && freq[cur] > k) {
-                int x = lower_bound(v.begin(), v.end(), nums[l]) - v.begin();
+                int x = id[l];
                 freq[x]--, l++;
             }
             ans = max(ans, r-l+1);
